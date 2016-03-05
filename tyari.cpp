9daab3fa9c4451966474003a25b3dd6be08037d6
navinder <navinder@buffalo.edu>
2016-03-04 19:36:40 -0500

    esds

---
+++ tyari.cpp
@@ -0,0 +1,12 @@
+// tyari.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+   std::cout<<"Welcome"<<std::endl;
+	return 0;   
+}
+
